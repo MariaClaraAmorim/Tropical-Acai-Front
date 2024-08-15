18a6c0554efd48909c162830c84bdce898a628ab
Maria Clara Amorim <mariaclaraamorim221@gmail.com>
2024-08-14 21:32:15 -0300

    space

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
     return (
         <div className="home">
             <img src={logo} alt="Tropical Açaí Logo" />
-            <h1>Bem-vindo(a) ao nosso catálogo,{clientName}!</h1>
+            <h1>Bem-vindo(a) ao nosso catálogo, {clientName}!</h1>
             <h2>Peça o seu açaí com a <br /><strong>Tropical Açaí</strong></h2>
             <p>Descubra nossos produtos incríveis e faça seu pedido agora mesmo!</p>
             {userRole === 'ADMIN' ? (
